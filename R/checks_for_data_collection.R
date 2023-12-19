bbeaa9ad40f55825fbaa2922cad0eda6292d287e
ACTED\Getu Gari <getu.boja22@yahoo.com>
2023-12-19 15:37:43 +0300

    remove second round test data

--- R/checks_for_data_collection.R
+++ R/checks_for_data_collection.R
@@ -78,6 +78,27 @@ df_testing_data <- df_tool_data |>
 
 add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_testing_data")
 
+df_testing_data2 <- df_tool_data |> 
+  filter(i.check.start_date == as_date("2023-12-15")) |> 
+  mutate(i.check.type = "remove_survey",
+         i.check.name = "",
+         i.check.current_value = "",
+         i.check.value = "",
+         i.check.issue_id = "logic_c_testing_data",
+         i.check.issue = "testing_data",
+         i.check.other_text = "",
+         i.check.checked_by = "GG",
+         i.check.checked_date = as_date(today()),
+         i.check.comment = "", 
+         i.check.reviewed = "1",
+         i.check.adjust_log = "",
+         i.check.so_sm_choices = "") |> 
+  dplyr::select(starts_with("i.check.")) |> 
+  rename_with(~str_replace(string = .x, pattern = "i.check.", replacement = "")) |> 
+  mutate(hh_kebele = as.character(hh_kebele))
+
+add_checks_data_to_list(input_list_name = "checks_output", input_df_name = "df_testing_data2")
+
 # fix enumerator_id data ------------------------------------------------------
 
 df_logic_c_enumerator_id_harmonization <- df_tool_data |> 
